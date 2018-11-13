04a21a2e4de53c15ce58b513d47dbb093ddd81d6
Daniel Brintzinger <dbrintzi@dbrintzi.remote.csb>
2018-11-13 14:24:06 +0100

    linebreak

--- get_oc_client.sh
+++ get_oc_client.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
 echo 'Downloading oc client'
-wget 
-https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-
-server-v3.9.0-191fece-linux-64bit.tar.gz -P /home/user/
+wget https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-server-v3.9.0-191fece-linux-64bit.tar.gz -P /home/user/
 echo 'Unpacking oc client'
-tar xfvz /home/user/openshift-origin-server-v3.9.0-191fece-linux-64bit.tar.gz -C 
-/projects
-mv  /projects/openshift-origin-server-v3.9.0-191fece-linux-64bit 
-/projects/ocpclient
+tar xfvz /home/user/openshift-origin-server-v3.9.0-191fece-linux-64bit.tar.gz -C /projects
+mv  /projects/openshift-origin-server-v3.9.0-191fece-linux-64bit /projects/ocpclient
 echo 'Adding oc to Path'
 export PATH=$PATH:/projects/ocpclient/
 echo 'Testing oc client'
