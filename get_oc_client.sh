d0132f8cc6d156dd38e71638e4a36da97b959a96
Daniel Brintzinger <dbrintzi@dbrintzi.remote.csb>
2018-11-13 14:15:22 +0100

    oc client retrieval script

---
+++ get_oc_client.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo 'Downloading oc client'
+wget 
+https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-
+server-v3.9.0-191fece-linux-64bit.tar.gz -P /home/user/
+echo 'Unpacking oc client'
+tar xfvz /home/user/openshift-origin-server-v3.9.0-191fece-linux-64bit.tar.gz -C 
+/projects
+mv  /projects/openshift-origin-server-v3.9.0-191fece-linux-64bit 
+/projects/ocpclient
+echo 'Adding oc to Path'
+export PATH=$PATH:/projects/ocpclient/
+echo 'Testing oc client'
+oc
+echo 'Removing tar file'
+rm /home/user/openshift-origin-server-v3.9.0-191fece-linux-64bit.tar.gz
+echo 'Done'
